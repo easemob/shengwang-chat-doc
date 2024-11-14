a9f6315cdee6b12ac317cf9e93b6f50f5db5b7f1
haoxiuwen <amy.hao@easemob.com>
2024-11-14 18:18:15 +0800

    modify

--- docs/.vuepress/sidebar/document.ts
+++ docs/.vuepress/sidebar/document.ts
@@ -374,7 +374,7 @@ const documentSidebar = [
         text: '离线推送',
         children: [
           { text: '离线推送设置', link: 'push.html' },
-          { text: '离线推送的消息扩展', link: 'push_extension.html' }
+          { text: '离线推送的消息扩展', link: 'push_extension.html' },
           { text: '查询离线推送结果', link: 'push_result_statistics.html' }
         ]
       },
