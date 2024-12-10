5241ceea6eda99f3de208607a7e4d02be3f0eef1
haoxiuwen <amy.hao@easemob.com>
2024-12-10 16:22:13 +0800

    modify

--- docs/.vuepress/sidebar/uikit.ts
+++ docs/.vuepress/sidebar/uikit.ts
@@ -225,7 +225,8 @@ const chatUikitSidebar = [
     only: ['Uniapp']
   },
   { text: "集成单群聊 UIKit", link: "chatuikit_integrated.html", only: ['Uniapp'] },
-  { text: "设计指南", link: "design_guide.html", only: ['Uniapp']}
+  { text: "设计指南", link: "design_guide.html", only: ['Uniapp'] },
+]  
 const chatroomUikitSidebar = [
   { text: "概述", link: "roomuikit_overview.html" },
   {
