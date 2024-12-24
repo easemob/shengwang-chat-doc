1d08880559aacd329e624c40808121f4ea158c8d
haoxiuwen <amy.hao@easemob.com>
2024-12-24 18:41:52 +0800

    Modify IM Doc Architecture

--- docs/.vuepress/sidebar/document.ts
+++ docs/.vuepress/sidebar/document.ts
@@ -6,6 +6,56 @@ const DOC_PATH = path.resolve(__dirname, '../../document')
 const platformList = getSubDirectories(DOC_PATH)
 
 const documentSidebar = [
+  { text: '文档指引', link: 'document_index.html' },
+  {
+    text: "产品介绍",
+    children: [
+      { text: '发版说明', link: 'releasenote.html'},
+      { text: "特性介绍",
+        collapsible: true,
+        children: [
+          { text: "消息", 
+            collapsible: true,
+            children: [
+              { text: "消息概述", link: "product_message_overview.html" },
+              { text: "消息格式", link: "product_message_format.html" },
+            ],
+          },
+          { text: "群组", 
+            collapsible: true,
+            children: [
+              { text: "群组概述", link: "product_group_overview.html" },
+              { text: "子区", link: "product_thread_overview.html" },
+            ],
+          },
+          { text: "聊天室", link: "product_chatroom_overview.html" },
+          { text: "用户相关", 
+            collapsible: true,
+            children: [
+              { text: "用户注册与登录", link: "product_user_registration_login.html" },
+              { text: "在线状态管理", link: "product_user_presence.html" },
+              { text: "用户属性", link: "product_user_attribute.html" },
+              { text: "用户关系", link: "product_user_relationship.html" },
+            ],
+          },
+          { text: "离线推送", link: "product_offline_push_overview.html" },
+        ], 
+      },
+      {
+        text: '计费说明',
+        collapsible: true,
+        children: [
+          { text: '计费策略', link: 'billing_strategy.html' },
+          { text: '计量策略', link: 'billing_rules.html' },
+        ]
+      },
+      { text: "平台支持", link: "platform_support.html" , except: ['server-side']},
+      { text: "数据中心", link: "data_center.html" },
+      { text: "IM 使用限制", link: "limitation.html" },
+      { text: "接口调用频率限制", link: "limitationapi.html" , only: ['server-side']},
+      { text: "术语表", link: "glossary.html" },
+    ],
+  },
   {
     /*
       text: 分组标题
@@ -20,19 +70,14 @@ const documentSidebar = [
     */
     text: '快速开始',
     children: [
-      { text: 'React Demo（WebIM）体验', link: 'demo_react.html', only: ['web'] },
-      { text: 'Vue Demo（WebIM）体验', link: 'demo_vue.html', only: ['web'] },
-      { text: 'Demo（EaseIM App）体验', link: 'demo.html', except: ['web', 'windows', 'react-native', 'flutter', 'unity', 'server-side'] },
-      { text: '快速开始', link: 'quickstart.html', except: ['windows', 'react-native', 'flutter', 'unity', 'server-side'] },
-      { text: '快速开始', link: 'quickstart.html', only: ['windows', 'react-native', 'flutter', 'unity'] },
+      // { text: 'React Demo（WebIM）体验', link: 'demo_react.html', only: ['web'] },
+      // { text: 'Vue Demo（WebIM）体验', link: 'demo_vue.html', only: ['web'] },
+      // { text: 'Demo（EaseIM App）体验', link: 'demo.html', except: ['web', 'windows', 'react-native', 'flutter', 'unity', 'server-side'] },
+      { text: "开通服务", link: "enable_im.html" },
+      { text: '实现收发消息', link: 'quickstart.html', except: ['server-side'] },
       // { text: '私有云 SDK 集成配置', link: 'privatecloud.html', except: ['windows', 'server-side', 'react-native', 'flutter', 'unity'] },
-      { text: 'SDK 更新日志', link: 'releasenote.html', except: ['server-side']},
       /*{ text: 'API reference', link: 'apireference.html', only: ['android', 'ios', 'web', 'windows', 'react-native', 'flutter', 'unity']},*/
-      { text: '开通和配置服务 console', link: 'enable_and_configure_IM.html', only: ['server-side'] },
-      { text: '使用环信 App Token 鉴权', link: 'easemob_app_token.html', only: ['server-side'] },
-      { text: '使用环信 User Token 鉴权', link: 'easemob_user_token.html', only: ['server-side'] },
-      { text: 'IM 产品使用限制', link: 'limitation.html', only: ['server-side'] },
-      { text: '接口频率限制', link: 'limitationapi.html', only: ['server-side'] },
+      { text: '使用 Token 鉴权', link: 'token_authentication.html', only: ['server-side'] },
     ],
     except: ['applet', 'electron','linux']
   },
@@ -155,6 +200,7 @@ const documentSidebar = [
     ],
     except: ['applet','server-side','electron','linux']
   },
+  { text: 'API 参考', link: 'api_reference_overview.html' },
   {
     text: '错误排查',
     children: [
@@ -163,15 +209,16 @@ const documentSidebar = [
     ],
     except: ['applet', 'server-side','electron','linux']
   },
-  {
-    text: 'CallKit 使用指南',
-    children: [
-      { text: 'EaseCallKit 使用指南', link: 'easecallkit.html', only: ['android', 'ios'] },
-      { text: 'CallKit 使用指南', link: 'easecallkit.html', only: ['web'] },
-    ],
-    except: ['applet', 'server-side','electron','linux']
+  { text: '合规指南', link: 'security.html' },
+  // {
+  //  text: 'CallKit 使用指南',
+  //  children: [
+  //    { text: 'EaseCallKit 使用指南', link: 'easecallkit.html', only: ['android', 'ios'] },
+  //    { text: 'CallKit 使用指南', link: 'easecallkit.html', only: ['web'] },
+  //  ],
+   // except: ['applet', 'server-side','electron','linux']
 
-  },
+  //},
   {
     text: '苹果隐私策略',
     children: [
@@ -179,185 +226,145 @@ const documentSidebar = [
     ],
     except: ['applet', 'server-side','electron','linux']
   },
-  {
-    text: '精简版 SDK',
-    children: [
-      { text: '精简版 SDK 使用说明', link: 'elite_sdk.html' },
-    ],
-    only: ['android', 'ios']
-  },
-  {
-    text: '产品介绍',
-    children: [
-      { text: '环信小程序全平台解决方案', link: 'overview.html' },
-      { text: '小程序 SDK 更新日志', link: 'releasenote.html' },
-    ],
-    only: ['applet']
-  },
-  {
-    text: '用户指南',
-    children: [
-      {
-        text: '集成介绍',
-        collapsible: true,
-        children: [
-          { text: '微信小程序', link: 'wechat.html' },
-          { text: 'QQ 小程序', link: 'qq.html' },
-          { text: '百度小程序', link: 'baidu.html' },
-          { text: '抖音小程序', link: 'bytedance.html' },
-          { text: '支付宝小程序', link: 'alipay.html' },
-          { text: 'Uniapp 全平台', link: 'uniapp.html' },
-        ],
-      },
-      { text: '初始化', link: 'initialization.html' },
-      {
-        text: '登录',
-        collapsible: true,
-        children: [
-          { text: '登录介绍', link: 'login.html' },
-          { text: '连接', link: 'connection.html' },
-          { text: '多设备登录', link: 'multi_device.html' },
-        ],  
-      },
-      {
-        text: '消息管理',
-        collapsible: true,
-        children: [
-          { text: '消息概述', link: 'message_overview.html' },
-          { text: '发送和接收消息', link: 'message_send_receive.html' },
-          { text: '获取历史消息', link: 'message_retrieve.html' },
-          { text: '撤回消息', link: 'message_recall.html' },
-          { text: '消息回执', link: 'message_receipt.html' }, 
-          { text: '消息表情回复', link: 'reaction.html' },
-          { text: '修改消息', link: 'message_modify.html' },
-          { text: '删除消息', link: 'message_delete.html' },
-          { text: '置顶消息', link: 'message_pin.html' }, 
-          { text: '翻译消息', link: 'message_translation.html' },
-          { text: '只投在线用户', link: 'message_deliver_only_online.html'},  
-          { text: '消息审核（举报）', link: 'moderation.html'},      
-        ]
-      },
-      {
-        text: '会话管理',
-        collapsible: true,
-        children: [
-          { text: '会话介绍', link: 'conversation_overview.html' },
-          { text: '会话列表', link: 'conversation_list.html' },
-          { text: '会话未读数', link: 'conversation_unread.html' },
-          { text: '置顶会话', link: 'conversation_pin.html' },
-          { text: '会话标记', link: 'conversation_mark.html'},
-          { text: '删除会话', link: 'conversation_delete.html'},
-        ]
-      },
-      {
-        text: '群组管理',
-        collapsible: true,
-        children: [
-          { text: '群组概述', link: 'group_overview.html' },
-          { text: '创建和管理群组', link: 'group_manage.html' },
-          { text: '管理群组成员', link: 'group_members.html' },
-          { text: '管理群组属性', link: 'group_attributes.html' },
-          {
-            text: '子区管理',
-            collapsible: true,
-            children: [
-              { text: '管理子区', link: 'thread.html' },
-              { text: '管理子区消息', link: 'thread_message.html' }
-            ]
-          },
-        ]
-      },
-      {
-        text: '聊天室管理',
-        collapsible: true,
-        children: [
-          { text: '聊天室概述', link: 'room_overview.html' },
-          { text: '创建和管理聊天室', link: 'room_manage.html' },
-          { text: '管理聊天室成员', link: 'room_members.html' },
-          { text: '管理聊天室属性', link: 'room_attributes.html' },
-        ]
-      },
-      {
-        text: '用户相关',
-        collapsible: true,
-        children: [
-          { text: '用户关系', link: 'user_relationship.html' },
-          { text: '用户属性', link: 'userprofile.html' },
-          { text: '在线状态订阅', link: 'presence.html' },
-        ]
-      },    
-      { text: '离线推送', 
-        collapsible: true,
-        children: [
-        { text: '离线推送概述', link: 'push/push_overview.html' }, 
-        { text: '设置通知方式和免打扰', link: 'push/push_notification_mode_dnd.html' },
-        { text: '设置推送模板', link: 'push/push_template.html' },
-        { text: '设置推送翻译', link: 'push/push_translation.html' },
-        { text: '设置推送扩展功能', link: 'push/push_extension.html' },
-        { text: 'uni-app 离线推送', link: 'push/uniapp_push.html' }
-        ]
-      }, 
-    ],
-    only: ['applet']
-  },
-  {
-    text: '错误排查',
-    children: [
-      { text: '错误码', link: 'error.html' },
-      { text: '日志', link: 'log.html' },
-    ],
-    only: ['applet']
-  },
-  {
-    text: '其他帮助',
-    children: [
-      { text: '小程序 API 文档', link: 'apidoc.html' },
-      { text: 'Uniapp 生成原生 Android、iOS 应用', link: 'uniappnativeapp.html' },
-      { text: '小程序模板使用指南', link: 'uniappuikit.html' },
-      { text: '如何配置服务器域名', link: 'serverconfig.html' },
-    ],
-    only: ['applet']
-  },
-  {
-    text: 'SDK 集成介绍',
-    children: [
-      { text: '集成说明', link: 'overview.html' },
-      { text: 'SDK 基础功能', link: 'sdk_basic_function.html' },
-      { text: 'Demo 下载体验', link: 'demo_download.html' },
-      { text: '桌面 SDK API 文档', link: 'sdk_api.html' },
-    ],
-    only: ['electron']
-  },
-  {
-    text: 'IM 基本功能',
-    children: [
-      { text: '消息', link: 'message.html' },
-      { text: '会话管理', link: 'conversation.html' },
-      { text: '用户关系管理', link: 'contact.html' },
-      { text: '群组', link: 'group.html' },
-      { text: '聊天室', link: 'chatroom.html' },
-      { text: '多设备监听', link: 'multi_device.html' },
-      { text: '附录', link: 'annex.html' },
-    ],
-    only: ['electron']
-  },
-  {
-    text: '更新日志',
-    children: [
-      { text: 'SDK 更新日志', link: 'releasenote.html' },
-    ],
-    only: ['electron']
-  },
-  {
-    text: 'Linux SDK 集成介绍',
-    children: [
-      { text: 'Linux SDK 集成说明', link: 'overview.html' },
-      { text: '技术参数', link: 'technical_parameter.html' },
-      { text: 'Linux SDK 更新日志', link: 'releasenote.html' },
-    ],
-    only: ['linux']
-  },
-  {
+  //{
+  //  text: '精简版 SDK',
+  //  children: [
+  //    { text: '精简版 SDK 使用说明', link: 'elite_sdk.html' },
+  //  ],
+  //  only: ['android', 'ios']
+  //},
+  // {
+  // text: '产品介绍',
+  //  children: [
+  //    { text: '环信小程序全平台解决方案', link: 'overview.html' },
+  //    { text: '小程序 SDK 更新日志', link: 'releasenote.html' },
+  //  ],
+  //  only: ['applet']
+  //},
+  //{
+  //  text: '用户指南',
+  // children: [
+  //    {
+  //      text: '集成介绍',
+  //      collapsible: true,
+  //      children: [
+  //        { text: '微信小程序', link: 'wechat.html' },
+  //        { text: 'QQ 小程序', link: 'qq.html' },
+  //        { text: '百度小程序', link: 'baidu.html' },
+  //        { text: '抖音小程序', link: 'bytedance.html' },
+  //        { text: 'Uniapp 全平台', link: 'uniapp.html' },
+  //      ],
+  //    },
+  //    { text: '初始化', link: 'initialization.html' },
+  //    {
+  //      text: '登录',
+  //      collapsible: true,
+  //      children: [
+  //        { text: '登录介绍', link: 'login.html' },
+  //        { text: '连接', link: 'connection.html' },
+  //        { text: '多设备登录', link: 'multi_device.html' },
+  //      ],  
+  //    },
+  //    {
+  //      text: '消息管理',
+  //      collapsible: true,
+  //      children: [
+  //        { text: '消息概述', link: 'message_overview.html' },
+  //        { text: '发送和接收消息', link: 'message_send_receive.html' },
+  //        { text: '获取历史消息', link: 'message_retrieve.html' },
+  //        { text: '撤回消息', link: 'message_recall.html' },
+  //        { text: '消息回执', link: 'message_receipt.html' }, 
+  //        { text: '消息表情回复', link: 'reaction.html' },
+  //        { text: '修改消息', link: 'message_modify.html' },
+  //        { text: '删除消息', link: 'message_delete.html' },
+  //        { text: '置顶消息', link: 'message_pin.html' }, 
+  //        { text: '翻译消息', link: 'message_translation.html' },
+  //        { text: '只投在线用户', link: 'message_deliver_only_online.html'},  
+  //        { text: '消息审核（举报）', link: 'moderation.html'},      
+  //      ]
+  //    },
+  //    {
+  //      text: '会话管理',
+  //      collapsible: true,
+  //      children: [
+  //        { text: '会话介绍', link: 'conversation_overview.html' },
+  //        { text: '会话列表', link: 'conversation_list.html' },
+  //        { text: '会话未读数', link: 'conversation_unread.html' },
+  //        { text: '置顶会话', link: 'conversation_pin.html' },
+  //        { text: '会话标记', link: 'conversation_mark.html'},
+  //        { text: '删除会话', link: 'conversation_delete.html'},
+  //      ]
+  //    },
+  //    {
+  //      text: '群组管理',
+  //      collapsible: true,
+  //      children: [
+  //        { text: '群组概述', link: 'group_overview.html' },
+  //        { text: '创建和管理群组', link: 'group_manage.html' },
+  //        { text: '管理群组成员', link: 'group_members.html' },
+  //        { text: '管理群组属性', link: 'group_attributes.html' },
+  //        {
+  //          text: '子区管理',
+  //          collapsible: true,
+  //          children: [
+  //            { text: '管理子区', link: 'thread.html' },
+  //            { text: '管理子区消息', link: 'thread_message.html' }
+  //          ]
+  //        },
+  //      ]
+  //    },
+  //    {
+  //      text: '聊天室管理',
+  //      collapsible: true,
+  //      children: [
+  //        { text: '聊天室概述', link: 'room_overview.html' },
+  //        { text: '创建和管理聊天室', link: 'room_manage.html' },
+  //        { text: '管理聊天室成员', link: 'room_members.html' },
+  //        { text: '管理聊天室属性', link: 'room_attributes.html' },
+  //      ]
+  //    },
+  //    {
+  //      text: '用户相关',
+  //      collapsible: true,
+  //      children: [
+  //        { text: '用户关系', link: 'user_relationship.html' },
+  //        { text: '用户属性', link: 'userprofile.html' },
+  //        { text: '在线状态订阅', link: 'presence.html' },
+  //      ]
+  //    },    
+  //    { text: '离线推送', 
+  //      collapsible: true,
+  //      children: [
+  //      { text: '离线推送概述', link: 'push/push_overview.html' }, 
+  //      { text: '设置通知方式和免打扰', link: 'push/push_notification_mode_dnd.html' },
+  //      { text: '设置推送模板', link: 'push/push_template.html' },
+  //      { text: '设置推送翻译', link: 'push/push_translation.html' },
+  //      { text: '设置推送扩展功能', link: 'push/push_extension.html' },
+  //      { text: 'uni-app 离线推送', link: 'push/uniapp_push.html' }
+  //      ]
+  //    }, 
+  //  ],
+  //  only: ['applet']
+  //},
+  //{
+  //  text: '错误排查',
+  //  children: [
+  //    { text: '错误码', link: 'error.html' },
+  //    { text: '日志', link: 'log.html' },
+  //  ],
+  //  only: ['applet']
+  //},
+  //{
+  //  text: '其他帮助',
+  //  children: [
+  //    { text: '小程序 API 文档', link: 'apidoc.html' },
+  //    { text: 'Uniapp 生成原生 Android、iOS 应用', link: 'uniappnativeapp.html' },
+  //    { text: '小程序模板使用指南', link: 'uniappuikit.html' },
+  //    { text: '如何配置服务器域名', link: 'serverconfig.html' },
+  //  ],
+  //  only: ['applet']
+  //},
+    {
     text: '服务端 REST API',
     children: [
       { text: '即时通讯 REST API 概览', link: 'overview.html' },
